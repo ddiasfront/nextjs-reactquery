315c9209cec55d181149d4b82653f0389bb44bc5
Diego Dias <diegodias@Diegos-MacBook-Pro.local>
2023-01-29 19:11:00 -0300

    first commit

---
+++ src/pages/posts.tsx
@@ -0,0 +1,37 @@
+// pages/posts.jsx
+import axios from "axios";
+import { dehydrate, QueryClient, useQuery } from "react-query";
+
+async function getPosts() {
+  const { data } = await axios.get("https://swapi.dev/api/people/1");
+  console.log(data);
+  return data;
+}
+
+export async function getStaticProps() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery("posts", getPosts);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
+
+function Posts() {
+  // This useQuery could just as well happen in some deeper child to
+  // the "Posts"-page, data will be available immediately either way
+  const { data } = useQuery("posts", getPosts);
+
+  // This query was not prefetched on the server and will not start
+  // fetching until on the client, both patterns are fine to mix
+  //   const { data: otherData } = useQuery("posts-2", getPosts);
+
+  console.log(data);
+
+  // ...
+}
+
+export default Posts;
